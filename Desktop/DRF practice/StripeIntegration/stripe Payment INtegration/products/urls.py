204432e454fe3b1edff8535f1d59b252af86af55
Tejashree5112 <tejashreesayankar@gmail.com>
2025-03-28 12:05:13 +0530

    updated

---
+++ Desktop/DRF practice/StripeIntegration/stripe Payment INtegration/products/urls.py
@@ -0,0 +1,22 @@
+
+from django.urls import path
+
+from .views import (
+    ProductDetailView,
+    ProductListView,
+    CreateStripeCheckoutSessionView,
+    SuccessView,
+    CancelView,
+    StripeWebhookView,
+)
+
+app_name = "products"
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="product-list"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("create-checkout-session/<int:pk>/", CreateStripeCheckoutSessionView.as_view(), name="create-checkout-session",),
+    path("success/", SuccessView.as_view(), name="success"),
+    path("cancel/", CancelView.as_view(), name="cancel"),
+    path("webhooks/stripe/", StripeWebhookView.as_view(), name="stripe-webhook"),
+]
